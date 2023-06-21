a4c72197eb33190dfd2073218884a5f0e7158ecd
aynur.abbaszadehh@gmail.com <runadyan370>
2023-06-21 17:32:14 +0400

    ok1

--- app/src/main/java/com/ayn/weatherapp/service/WeatherService.kt
+++ app/src/main/java/com/ayn/weatherapp/service/WeatherService.kt
@@ -1,10 +1,11 @@
 package com.ayn.weatherapp.service
 
 import com.ayn.weatherapp.model.WeatherModel
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 
 interface WeatherService {
-
+   @GET("weather")
     suspend fun getWeather(@Query("id") id : Int) : WeatherModel
 }
